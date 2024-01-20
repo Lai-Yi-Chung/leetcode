f7bc79a3175b699d4582741721c31a9e20ea01fc
jack <jack.me03@nctu.edu.tw>
2024-01-20 16:41:22 +0800

    input [4,5] val:4 fail

--- 27.remove_element.cpp
+++ 27.remove_element.cpp
@@ -2,19 +2,36 @@
 #include <vector>
 class Solution {
 public:
-    int removeElement(std::vector<int>& nums, int val) {
+    int removeElement(vector<int>& nums, int val) {
         int left_index = 0;
         int right_index = nums.size()-1;
+        if(right_index == left_index){
+            if(nums[right_index]==val)
+                return 0;
+            else
+                return 1;
+        }
+        //if((nums.size() == 2)&&(nums[1]==3)&&(val==5))
+        //    return 2;
+            
+
+
         while(right_index > left_index){
-            if(nums[left_index] != val)
+            if((right_index >= left_index)&&(nums[left_index] != val))
                 left_index++;
             else{
-                while(nums[right_index] == val){
+                while((right_index > left_index)&&(nums[right_index] == val)){
                     right_index--;
                 }
-                nums[left_index] = nums[right_index];
+                nums[left_index]  = nums[right_index];
+                nums[right_index] = val; 
             }
         }
+        // if(nums[left_index]!=val)
+        //     left_index++;
+        if(left_index == (nums.size()-1))
+            left_index++;
+
         return left_index;
     }
 };
