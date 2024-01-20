70d6fec79e7ef0afe5fcb285b2f023141b8076fc
jack <jack.me03@nctu.edu.tw>
2024-01-20 17:13:18 +0800

    input [3,2,2,3] val:3 fail

--- 27.remove_element.cpp
+++ 27.remove_element.cpp
@@ -11,27 +11,19 @@ public:
             else
                 return 1;
         }
-        //if((nums.size() == 2)&&(nums[1]==3)&&(val==5))
-        //    return 2;
-            
 
 
         while(right_index > left_index){
-            if((right_index >= left_index)&&(nums[left_index] != val))
+            if((right_index > left_index)&&(nums[left_index] != val))
                 left_index++;
             else{
                 while((right_index > left_index)&&(nums[right_index] == val)){
                     right_index--;
                 }
-                nums[left_index]  = nums[right_index];
-                nums[right_index] = val; 
             }
+            nums[left_index]  = nums[right_index];
+            nums[right_index] = val; 
         }
-        // if(nums[left_index]!=val)
-        //     left_index++;
-        if(left_index == (nums.size()-1))
-            left_index++;
-
         return left_index;
     }
 };
