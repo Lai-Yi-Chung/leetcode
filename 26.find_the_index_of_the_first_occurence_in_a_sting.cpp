6fed6c9318da0959fa50fe178a75efd896fca62f
jack <jack.me03@nctu.edu.tw>
2024-01-20 20:52:21 +0800

    fail

---
+++ 26.find_the_index_of_the_first_occurence_in_a_sting.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int needle_size = needle.size();
+        int needle_count = 0;
+        for(std::string::size_type i = 0; i <= (haystack.size()-needle.size()); i++){
+        //for(size_t i = 0; i <= (haystack.size()-needle.size()); i++){  
+            for(std::string::size_type j = 0; j < needle.size(); j++){
+                if(haystack.at(i+j) == needle.at(j))
+                    needle_count++;
+            }
+            if(needle_size == needle_count)
+                return i;
+        }
+        return -1;
+    }
+};
+
+int main (){
+    Solution solution;
+    cout << solution.strStr("hello","ll") << endl;
+    return 0;
+}
