3268228eb4c9ba6e536ff65cd34bc5fdb105212f
jack <jack.me03@nctu.edu.tw>
2024-01-21 14:42:52 +0800

    fail or exccced time limit

---
+++ 24.swap_nodes_in_pairs.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* last_last_node = nullptr;
+        ListNode* last_node = nullptr;
+        ListNode* current = head;
+        int swap_count = 0;
+        while(current->next){
+            swap_count++;
+            if(swap_count == 1){
+                last_last_node = current;
+                last_node = current->next;
+            }else if(swap_count == 2){
+                swap_count = 0;
+                last_last_node->next = current->next;
+                last_node->next = current->next->next;
+                //current->next->next = last_node;
+                //current->next = current;
+                //current->next->next = current;
+            }
+            current = current->next;
+        }    
+        return head;
+    }
+};
